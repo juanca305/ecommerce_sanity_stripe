bac263b759ad248085dd3bf04f8e3b587ebc3e40
Juan Carlos <jc.rdguez64@gmail.com>
2023-11-27 09:56:37 -0500

    AddToCart OK

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+
 import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+    
     
   )
 }
