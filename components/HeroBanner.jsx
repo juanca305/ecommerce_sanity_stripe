c8bdd26c14772a681012d7760fab97bb0f86aa7d
Juan Carlos Rodriguez <jc.rdguez64@gmail.com>
2023-12-15 07:39:09 -0500

    Payment Ready starting Buy Now button

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,24 +1,28 @@
 
+
 import React from 'react';
 import Link from 'next/link';
+
 import { urlFor } from '../lib/client';
 
 const HeroBanner = ({ heroBanner }) => {
- 
+  console.log('MYHEROBANNER_NEW', heroBanner);
+  console.log('GTYPEOFIMG', typeof(heroBanner.image));
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
       <div>
-        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image'/>
+        <img src={urlFor(heroBanner.image)} className="hero-banner-image" />
+
         <div>
           <Link href={`/product/${heroBanner.product}`}>
-              <button type='button'>{heroBanner.buttonText}</button>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
-          <div className='desc'>
-              <h5>Description</h5>
-              <p>{heroBanner.desc}</p>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
